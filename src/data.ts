061d9c1d69d1a1a0d1de9992707aad7c5d7163ec
Rintaro <sirime2351develop@gmail.com>
2026-08-11 23:35:00 +0900

    更新

--- src/data.ts
+++ src/data.ts
@@ -318,7 +318,7 @@ export const timelineEvents: TimelineEvent[] = [
   {
     year: '2026.August',
     title: 'MEDIA DO Co., Ltd.',
-    description: '電子書籍の配信・管理を支えるシステムのインターンへ参加。システムのパフォーマンスチューニングに取り組みました。',
+    description: '電子書籍の配信・管理を支えるシステムの4Daysインターンへ参加。システムのパフォーマンスチューニングに取り組みました。',
     image: 'https://mediado.jp/wp-content/themes/basic-theme/img/common/logo_ogp.png',
     links: [
       {
